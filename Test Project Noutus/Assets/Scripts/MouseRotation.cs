0f94ed31d4b870c66df5c48f9bb6a37129a54a85
Noutus <arnout_verburg@hotmail.com>
2017-09-16 11:46:46 +0200

    add mouse rotation

---
+++ Test Project Noutus/Assets/Scripts/MouseRotation.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseRotation : MonoBehaviour
+{
+    private Plane plane;
+
+    void Start()
+    {
+        this.plane = new Plane(Vector3.up, Vector3.zero);
+    }
+
+    void FixedUpdate()
+    {
+        float distance = 0;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (this.plane.Raycast(ray, out distance))
+        {
+            var targetDirection = ray.GetPoint(distance) - this.transform.position;
+            var newDirection = Vector3.RotateTowards(transform.forward, targetDirection, 1, 0);
+            this.transform.rotation = Quaternion.LookRotation(newDirection);
+        }
+	}
+}
