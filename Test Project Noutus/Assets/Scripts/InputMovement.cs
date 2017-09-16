b50e9b47b2e709292df70804057e08c7d5d8d85f
Noutus <arnout_verburg@hotmail.com>
2017-09-16 11:23:39 +0200

    add input movement

---
+++ Test Project Noutus/Assets/Scripts/InputMovement.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputMovement : MonoBehaviour
+{
+    public float Speed = 0.33f;
+
+	void FixedUpdate()
+    {
+        var horizontalMovement = Input.GetAxis(InputStrings.Horizontal) * this.Speed;
+        var verticalMovement = Input.GetAxis(InputStrings.Vertical) * this.Speed;
+
+        this.transform.Translate(horizontalMovement, 0, 0);
+        this.transform.Translate(0, 0, verticalMovement);
+	}
+}
